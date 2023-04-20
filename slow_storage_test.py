7af588441ce8dab13dda7238425e4ae57bb16835
Rahul Trada <rahult@graphcore.ai>
2023-04-20 17:26:47 +0100

    comment out md5sum tests

--- slow_storage_test.py
+++ slow_storage_test.py
@@ -12,28 +12,28 @@ for env in env_vars:
 
 
 
-file1, file2 = "13800232677581770137.popef", "9028179549235720115.popef"
-dataset_folder = "/datasets/poplar-executables-hf-3-2/stablediffusion2_text2img"
-tmp_folder = "/tmp/exe_cache/stablediffusion2_text2img"
+# file1, file2 = "13800232677581770137.popef", "9028179549235720115.popef"
+# dataset_folder = "/datasets/poplar-executables-hf-3-2/stablediffusion2_text2img"
+# tmp_folder = "/tmp/exe_cache/stablediffusion2_text2img"
 
-# 2,3. Test first two read speed - /datasets
-for i in [1, 2]:
-    print(f"Read speed test {i} from /datasets - start")
-    cmd = f"md5sum -b {dataset_folder}/{file1} {dataset_folder}/{file2}".split()
-    start_time = time.time()
-    output = subprocess.check_output(cmd)
-    end_time = time.time()
-    duration = end_time - start_time
-    print(f"Duration(s): {duration:.2f}")
-    print(f"Read speed test {i} from /datasets - end")
-    print("-"*20)
+# # 2,3. Test first two read speed - /datasets
+# for i in [1, 2]:
+#     print(f"Read speed test {i} from /datasets - start")
+#     cmd = f"md5sum -b {dataset_folder}/{file1} {dataset_folder}/{file2}".split()
+#     start_time = time.time()
+#     output = subprocess.check_output(cmd)
+#     end_time = time.time()
+#     duration = end_time - start_time
+#     print(f"Duration(s): {duration:.2f}")
+#     print(f"Read speed test {i} from /datasets - end")
+#     print("-"*20)
 
-# 4. Test read speed - /tmp
-print("Read speed test from /tmp - start")
-cmd = f"md5sum -b {tmp_folder}/{file1} {tmp_folder}/{file2}".split()
-start_time = time.time()
-output = subprocess.check_output(cmd)
-end_time = time.time()
-duration = end_time - start_time
-print(f"Duration(s): {duration:.2f}")
-print("Read speed test from /tmp - end")
+# # 4. Test read speed - /tmp
+# print("Read speed test from /tmp - start")
+# cmd = f"md5sum -b {tmp_folder}/{file1} {tmp_folder}/{file2}".split()
+# start_time = time.time()
+# output = subprocess.check_output(cmd)
+# end_time = time.time()
+# duration = end_time - start_time
+# print(f"Duration(s): {duration:.2f}")
+# print("Read speed test from /tmp - end")
